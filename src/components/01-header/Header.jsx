010f38cfea57a3c977648e4bb84a76bfbea29b56
unknown <insighttec.m@gmail.com>
2023-12-26 06:15:28 -0800

    finish header

--- src/components/01-header/Header.jsx
+++ src/components/01-header/Header.jsx
@@ -1,7 +1,112 @@
-import React from 'react'
-import './header.css'
-export default function Header() {
+import { useEffect, useState } from "react";
+import "./header.css";
+
+const Header = () => {
+  const [showModal, setshowModal] = useState(false);
+  const [theme, setTheme] = useState(
+    localStorage.getItem("currentMode") ?? "dark"
+  );
+
+  useEffect(() => {
+    if (theme === "light") {
+      document.body.classList.remove("dark");
+      document.body.classList.add("light");
+    } else {
+      document.body.classList.remove("light");
+      document.body.classList.add("dark");
+    }
+  }, [theme]);
+
   return (
-    <div>header</div>
-  )
-}
+    <header className="  flex">
+      <button
+        onClick={() => {
+          setshowModal(true);
+        }}
+        className="menu icon-menu flex"
+      >
+        {" "}
+      </button>
+      <div />
+
+      <nav>
+        <ul className="flex">
+          <li>
+            <a href="">About</a>
+          </li>
+
+          <li>
+              <a href="">Projects</a>
+            </li>
+            <li>
+              <a href="">Future Projects</a>
+            </li>
+            <li>
+              <a href="">Our Team</a>
+            </li>
+            <li>
+              <a href="">Technologies </a>
+            </li>
+            <li>
+              <a href="">Articles</a>
+            </li>
+        </ul>
+      </nav>
+
+      <button
+        onClick={() => {
+          // Send value to LS
+          localStorage.setItem(
+            "currentMode",
+            theme === "dark" ? "light" : "dark"
+          );
+
+          // get value from LS
+          setTheme(localStorage.getItem("currentMode"));
+        }}
+        className="mode flex"
+      >
+        {theme === "dark" ? (
+          <span className="icon-moon-o"> </span>
+        ) : (
+          <span className="icon-sun"> </span>
+        )}
+      </button>
+
+      {showModal && (
+        <div className="fixed">
+          <ul className="modal ">
+            <li>
+              <button
+                className="icon-close"
+                onClick={() => {
+                  setshowModal(false);
+                }}
+              />
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Projects</a>
+            </li>
+            <li>
+              <a href="">Future Projects</a>
+            </li>
+            <li>
+              <a href="">Our Team</a>
+            </li>
+            <li>
+              <a href="">Technologies </a>
+            </li>
+            <li>
+              <a href="">Articles</a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </header>
+  );
+};
+
+export default Header;

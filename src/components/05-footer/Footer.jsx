4857f39d3dcae5fc497725e37130a6e0538b06c9
unknown <insighttec.m@gmail.com>
2023-12-27 22:40:05 -0800

    add footer content

--- src/components/05-footer/Footer.jsx
+++ src/components/05-footer/Footer.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
-import './footer.css'
-export default function Footer() {
+import React from "react";
+import "./footer.css";
+const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="flex">
+      <ul className="flex">
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Projects</a>
+        </li>
+        <li>
+          <a href="">Services</a>
+        </li>
+        <li>
+          <a href="">Team</a>
+        </li>
+        <li>
+          <a href="">Technologies</a>
+        </li>
+        <li>
+          <a href="">Articles</a>
+        </li>
+      </ul>
+
+      <p>© 2023 InsightTec. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
